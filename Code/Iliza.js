91a3552bc4301783cdbc07217e8bb42103c4c348
MKaziel <mkaziel@hotmail.com>
2018-04-04 19:18:18 +0200

    mise à jour des éléments

--- Code/Iliza.js
+++ Code/Iliza.js
@@ -1,36 +1,48 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+var refuse = 0;
 
 bot.login('NDI0Njk5MzA0NjcwNjU4NTYw.DY8ryQ.1U_xhjvjDPBd40NCMcURaXb26dQ');
 
+// Application des paramètre et commande lorsque le bot est lancé
 bot.on("ready", function () {
+    //Affiché où est ce que le bot est connecté avec son tag
     console.log(`je suis connecté en tant que ${bot.user.tag}!`);
-    bot.user.setActivity("En cour de Dev");
+    //Définition de son activité
+    bot.user.setActivity("En cour de Dev").catch(console.error());
   })
-  
+ 
+// Fonction de réponse de base du bot, pour tester s'il fonctionne
 bot.on('message', function(message) {
     if (message.content === 'ping') {
         message.channel.send('Pong!');
     }
 })
 
+// fonction pour dire bonjour au bot et avoir une réponse en fonction de l'utilisateur
 bot.on('message', function(message) {    
     if(message.isMemberMentioned(bot.user))
     {
-        if(message.content.includes("Ohayo"))
+        if(message.content.toLocaleLowerCase.includes("ohayo"))
         {
-            message.reply(" Ohayo !");
+            message.channel.send("<@"+message.author.id+"> Ohayo !");
         }
     }       
 })
 
+// Fonction d'arrêt du bot
 bot.on("message", function(message) {
     if(message.content === "Iliza disconect")
     {
         if(message.author.id === "226367048266022913")
         {
             message.channel.send('Oyasuminasai <@226367048266022913> :3');
-            bot.destroy();
+            bot.destroy().catch(console.error());
+        }
+        else if(message.author.id === "249947643453636610" && refuse === 0)
+        {
+            message.channel.send("C'est bien parce que c'est toi <@249947643453636610>, Oyasuminasai  :)");
+            bot.destroy().catch(console.error());
         }
         else
         {
@@ -38,4 +50,21 @@ bot.on("message", function(message) {
         }
     }
 
+})
+
+// Modification de la variable refus pour activer/désactiver le mode refus
+bot.on("message", function(message){
+    if(message.author.id === "226367048266022913")
+    {
+        if(message.content.includes("refus") && message.content.includes("active")  && refuse === 0)
+        {
+            refuse = 1;
+            message.channel.send("La commande a été correctement exécutée, le mode refus a été activé");
+        }
+        if(message.content.includes("refus") && message.content.includes("désactive")  && refuse === 1)
+        {
+            refuse = 0;
+            message.channel.send("La commande a été correctement exécutée, le mode refus a été désactivé");
+        }
+    }
 })
